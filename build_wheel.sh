17e23d0378be8afc350b47fa38bf0d6cd4641534
Ryan Turner <rturner@twitter.com>
2020-07-22 17:36:11 -0700

    adding needed scripts

---
+++ build_wheel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+set -o pipefail
+
+# Display what version is being used for logging
+python --version
+
+# Fail if untracked files so we don't delete them in next step
+test -z "$(git status --porcelain)"
+
+# Build from clean repo, delete all ignored files
+git clean -x -f -d
+
+# Get everything in place to put inside the wheel
+SHA_LONG=$(git rev-parse HEAD)
+echo VERSION=\"$SHA_LONG\" >version.log
+
+# Now the actual build
+python3 setup.py sdist
---
+++ local_test.sh
@@ -0,0 +1,2 @@
+# For now, just run the regular tests, that's it
+pytest test/ -s -v --disable-pytest-warnings
