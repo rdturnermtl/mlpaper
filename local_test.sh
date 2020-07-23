de8f8e67a25491e9693c8490e436a0c43c779bae
Ryan Turner <rturner@twitter.com>
2020-07-22 19:47:22 -0700

    updating local test

--- local_test.sh
+++ local_test.sh
@@ -1,3 +1,21 @@
 # For now, just run the regular tests, that's it
 python --version
+
+PY=python3.7
+
+# Test using pinned
+virtualenv env --python=$PY
+source ./env/bin/activate
+pip install -r requirements/tools.txt
+pip install -r requirements/test.txt
+pip install -e .[test]
 pytest tests/ -s -v --disable-pytest-warnings
+deactivate
+
+# Test using latest
+virtualenv env --python=$PY
+source ./env/bin/activate
+pip install -r requirements/tools.txt
+pip install -e .[test]
+pytest tests/ -s -v --disable-pytest-warnings
+deactivate
