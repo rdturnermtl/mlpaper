0848a789cf1ee57a934c65e467e942131f03d6bc
Ryan Turner <rturner@twitter.com>
2020-07-23 20:54:09 -0700

    now int test passes

--- integration_test.sh
+++ integration_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#
+# For now, just run the demos, that's it
 
 set -ex
 set -o pipefail
 
-# For now, just run the demos, that's it
 PY=python3.7
 
 # Test using pinned
@@ -17,8 +18,8 @@ python demos/plot_regression_comparison.py
 deactivate
 
 # Test using latest
-virtualenv env --python=$PY
-source ./env/bin/activate
+virtualenv env_latest --python=$PY
+source ./env_latest/bin/activate
 python --version
 pip install -e .[demo]
 python demos/plot_classifier_comparison.py
