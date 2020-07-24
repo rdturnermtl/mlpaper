485ab8c00492819db23ff9d489286e1705d39536
Ryan Turner <rturner@twitter.com>
2020-07-23 20:43:53 -0700

    code coverage note

--- local_test.sh
+++ local_test.sh
@@ -1,20 +1,23 @@
+#!/bin/bash
+#
 # For now, just run the regular tests, that's it
-python --version
 
 PY=python3.7
 
 # Test using pinned
 virtualenv env --python=$PY
 source ./env/bin/activate
+python --version
 pip install -r requirements/tools.txt
 pip install -r requirements/test.txt
 pip install -e .[test]
-pytest tests/ -s -v --disable-pytest-warnings
+pytest tests/ -s -v --disable-pytest-warnings --cov=mlpaper --cov-report html
 deactivate
 
 # Test using latest
 virtualenv env --python=$PY
 source ./env/bin/activate
+python --version
 pip install -r requirements/tools.txt
 pip install -e .[test]
 pytest tests/ -s -v --disable-pytest-warnings
